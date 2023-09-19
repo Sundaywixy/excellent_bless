3d77ce08ed599f807a5c2d1cf8fe93a77acc407e
sundaywixy <sundayemmanuelng@aol.com>
2023-09-19 23:38:09 +0100

    files to be copied

---
+++ README.md
@@ -0,0 +1,4 @@
+this is the home made version of the printf function that prints to standard
+output. the printf project is a team project to foster team spirit and 
+encourages collaboration amongst software engineers.
+this project takes into consideration every aspect the printf function.
---
+++ utils.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * is_printable - function name
+ * @c: function parameter
+ * Return: printed strinmg
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - fuction name
+ * @buffer: buffer array
+ * @i: function parameter
+ * @ascii_code: code representation ascii
+ * Return: string printed
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - function name
+ * @c: function parameter
+ * Return: printed funtion
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - function name
+ * @num: declaredvariable
+ * @size: chacks for size
+ * Return: strings printted
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - function name
+ * @num: function format
+ * @size: returns string size
+ * Return: string to stdout
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
